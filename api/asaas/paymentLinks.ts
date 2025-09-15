3e068f4e47dbed4be68cad2adc01bd652918a4e4
centralcardapio <centralcardapio@gmail.com>
2025-09-15 11:08:57 -0300

    fix: corrigir conflito ES modules/CommonJS na Edge Function

--- api/asaas/paymentLinks.ts
+++ api/asaas/paymentLinks.ts
@@ -1,6 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: any, res: any) {
   // Configurar CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
